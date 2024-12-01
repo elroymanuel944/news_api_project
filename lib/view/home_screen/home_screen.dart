0842f4f6555d51f247619beb0342c407c3312956
elroymanuel944 <emcpara@gmail.com>
2024-12-01 22:33:28 +0530

    finsh

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:news_api_project/controller/new_api_controller.dart';
 import 'package:news_api_project/model/category_scetion.dart';
